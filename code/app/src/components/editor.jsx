27a221b97927556b56984eed07a07ccd279a6c91
wooh2 <ho.woo2@mail.dcu.ie>
2024-01-30 15:56:04 +0000

    Edited Editor

--- code/app/src/components/editor.jsx
+++ code/app/src/components/editor.jsx
@@ -1,9 +1,19 @@
-import { useEffect } from "react";
+import { useState } from 'react';
+import Element from './element';
 
 function Editor() {
+    const [component, setComponent] = useState(null);
+    function addElement() {
+        var div = document.createElement("div");
+        console.log(<Element id={3}/>);
+        div.innerHTML = <Element id={3}/>;
+        document.getElementById("Editor").appendChild(div);
+    }
+
     return (
-        <div>
+        <div id="Editor">
             <h1>Editor</h1>
+            <button onClick={addElement}>Add Element</button>
         </div>
     )
 }
